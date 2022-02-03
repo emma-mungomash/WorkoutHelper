78c76fbf52472271ace52b2320162bcd59141c7e
Emma Dalton <emma@mungomash.com>
2022-02-02 19:41:21 -0500

    Initial Commit

---
+++ WorkoutIntervalTimer/WorkoutIntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutIntervalTimerApp.swift
+//  WorkoutIntervalTimer
+//
+//  Created by Emma Dalton on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutIntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
